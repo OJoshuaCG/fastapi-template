982521d4a348ae14adf75baca3b5b4e41ee9b42e
Joshua <ojoshuacg@gmail.com>
2025-12-02 06:20:51 -0600

    feat. Middleware para realizar logs de las solicitudes http

--- app/utils/environments.py
+++ app/utils/environments.py
@@ -8,6 +8,11 @@ load_dotenv()
 ROOT_DIR = Path(__file__).parent.parent
 APP_DIR = ROOT_DIR / "app"
 
+# ======= Application variables ======= #
+APP_ENV = os.getenv("APP_ENV", "development")
+APP_NAME = os.getenv("APP_NAME", "FastAPI Project")
+SECRET_KEY = os.getenv("SECRET_KEY", "abcde-12345")
+
 # ======= Logger variables ======= #
 LOGGER_LEVEL = os.getenv("LOGGER_LEVEL", "INFO")
 LOGGER_MIDDLEWARE = os.getenv("LOGGER_MIDDLEWARE", "True").lower() == "true"
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+
+from app.middleware.LoggerMiddleware import LoggerMiddleware
+from app.utils.environments import SECRET_KEY
 
 app = FastAPI()
+app.add_middleware(LoggerMiddleware)
+
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 
 @app.get("/test/ping", tags=["test"])
