62629972c3363dc08cd7ce685054c2626fc7542c
Joshua Carrasco <ojoshuacg@gmail.com>
2026-02-11 14:53:19 -0600

    feat: Variables de entorno definidas en nuevo lugar (carpeta core)

--- app/core/environments.py
+++ app/core/environments.py
@@ -11,15 +11,14 @@ APP_DIR = ROOT_DIR / "app"
 # ======= Application variables ======= #
 APP_ENV = os.getenv("APP_ENV", "development")
 APP_NAME = os.getenv("APP_NAME", "FastAPI Project")
-SECRET_KEY = os.getenv("SECRET_KEY", "abcde-12345")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # ======= Logger variables ======= #
 LOGGER_LEVEL = os.getenv("LOGGER_LEVEL", "INFO")
 LOGGER_MIDDLEWARE = os.getenv("LOGGER_MIDDLEWARE", "True").lower() == "true"
-LOGGER_MIDDLEWARE_SHOW_HEADERS = (
-    os.getenv("LOGGER_MIDDLEWARE_SHOW_HEADERS", "False").lower() == "true"
-)
-LOGGER_EXCEPTIONS = os.getenv("LOGGER_EXCEPTIONS", "True").lower() == "true"
+LOGGER_MIDDLEWARE_SHOW_HEADERS = os.getenv("LOGGER_MIDDLEWARE_SHOW_HEADERS", "False").lower() == "true"
+LOGGER_EXCEPTIONS = os.getenv("LOGGER_EXCEPTIONS", "False").lower() == "true"
 
 
 # ======= Database variables ======= #
@@ -27,4 +26,4 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "username")
 DB_PASS = os.getenv("DB_PASS", "password")
 DB_NAME = os.getenv("DB_NAME", "database")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
